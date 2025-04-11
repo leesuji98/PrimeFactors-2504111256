b43953684545b7897d4173f07c2fff4df5b8327e
leesuji <leesuji98@naver.com>
2025-04-11 13:55:55 +0900

    Of3 add

--- Project23/prime-factory_test.cpp
+++ Project23/prime-factory_test.cpp
@@ -14,8 +14,12 @@ TEST_F(PrimeFixture, Of1) {
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-
 TEST_F(PrimeFixture, Of2) {
 	expected = { 2 };
 	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFixture, Of3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
