9d053317b75683141c519ae7961ca46c42f9dade
leesuji <leesuji98@naver.com>
2025-04-11 14:02:20 +0900

    of4 refactoring

--- Project23/prime-factory.cpp
+++ Project23/prime-factory.cpp
@@ -6,15 +6,17 @@ public:
 	vector <int> of(int number) {
 		vector<int> result = {};
 
-		if (number == 4) {
-			result.push_back(2);
-			result.push_back(2);
-		}
-
 		if (number > 1) {
-			result.push_back(number);
+			if (number == 4) {
+				while (number % 2 == 0) {
+					result.push_back(2);
+					number /= 2;
+				}
+			}
+			else {
+				result.push_back(number);
+			}
 		}
-
 		return result;
 	}
 };
