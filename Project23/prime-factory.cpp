d28378f04dc66b71d0fcbfe758b7f3e06c0359b3
leesuji <leesuji98@naver.com>
2025-04-11 13:52:20 +0900

    Of2 refactoring

--- Project23/prime-factory.cpp
+++ Project23/prime-factory.cpp
@@ -3,8 +3,12 @@ using std::vector;
 
 class PrimeFactor {
 public:
-	vector <int> of(int i) {
+	vector <int> of(int number) {
 		vector<int> result = {};
+
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
--- Project23/prime-factory_test.cpp
+++ Project23/prime-factory_test.cpp
@@ -3,9 +3,17 @@
 
 using namespace std;
 
-TEST(PrimeFators, PrimeTest) {
+TEST(PrimeFators, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+
+TEST(PrimeFators, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
